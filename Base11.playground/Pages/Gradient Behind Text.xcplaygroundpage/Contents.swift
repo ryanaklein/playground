fd31711f449a27982527badf43fd937382faa260
Ryan Klein <ryan.klein@outlook.com>
2018-01-15 02:50:43 -0500

    Somehow it's working?

--- Base11.playground/Pages/Gradient Behind Text.xcplaygroundpage/Contents.swift
+++ Base11.playground/Pages/Gradient Behind Text.xcplaygroundpage/Contents.swift
@@ -6,43 +6,55 @@ import UIKit
 
 var str = "Hello, playground"
 
-let textView = UIView(frame: CGRect(x: 0, y: 0, width: 300, height: 100))
-textView.backgroundColor = UIColor.white
+class CustomDrawingView: UIView {
+    
+    func imageView() -> CGImage?{
+        UIGraphicsBeginImageContext(self.bounds.size)
+        defer { UIGraphicsEndImageContext()}
+        
+        if let context = UIGraphicsGetCurrentContext() {
+            self.layer.render(in: context)
+            return context.makeImage()
+        }
+        return nil
+    }
+    
+    func imageViewImageMask() -> CGImage?{
+        UIGraphicsBeginImageContext(self.bounds.size)
+        defer { UIGraphicsEndImageContext()}
+        
+        if let context = UIGraphicsGetCurrentContext() {
+            self.layer.render(in: context)
+            let madeImage = context.makeImage()!
+            /*let madeImage = CGImage(width: context.width, height: context.height, bitsPerComponent: context.bitsPerComponent, bitsPerPixel: context.bitsPerPixel, bytesPerRow: context.bytesPerRow, space: CGColorSpaceCreateDeviceGray(), bitmapInfo: CGBitmapInfo(), provider: someImage.dataProvider!, decode: nil, shouldInterpolate: true, intent: .absoluteColorimetric)!
+            madeImage.alphaInfo.rawValue*/
+            return madeImage
+        }
+        return nil
+    }
+}
+
+let view = CustomDrawingView(frame: CGRect(x: 0, y: 0, width: 300, height: 100))
+view.backgroundColor = UIColor.white
+let image1 = view.imageView()!
 
 
+let label = UILabel(frame: view.frame)
+label.text = "Hello, World!"
+label.textColor = UIColor.white
+view.backgroundColor = UIColor.black
+view.addSubview(label)
+let image2 = view.imageViewImageMask()!
 
-let textLayer = CATextLayer()
-textLayer.bounds = CGRect(x:0, y:0, width:300, height:100)
-textLayer.position = textView.center
-textLayer.fontSize = 36
-textLayer.foregroundColor = UIColor.white.cgColor
-textLayer.backgroundColor = UIColor.clear.cgColor
-textLayer.string = "Hello World!"
+let mask = CGImage(maskWidth: image2.width, height: image2.height, bitsPerComponent: image2.bitsPerComponent, bitsPerPixel: image2.bitsPerPixel, bytesPerRow: image2.bytesPerRow, provider: image2.dataProvider!, decode: nil, shouldInterpolate: false)
 
-let clearLayer = CALayer()
-clearLayer.bounds = CGRect(x:0, y:0, width:300, height:100)
-clearLayer.position = textView.center
-clearLayer.backgroundColor = UIColor.white.cgColor
+let liveView = UIImageView(frame: CGRect(x: 0, y: 0, width: 300, height: 100))
+let image3 = image1.masking(mask!)!
 
+liveView.backgroundColor = UIColor.green
+liveView.image = UIImage(cgImage: image3)
 
-let gradientLayer = CAGradientLayer()
-gradientLayer.colors = [UIColor.orange.cgColor, UIColor.lightGray.cgColor]
-gradientLayer.bounds = CGRect(x: 0, y: 0, width: textView.bounds.width, height: textView.bounds.height)
-gradientLayer.position = CGPoint(x:(0.0 - gradientLayer.bounds.width/2), y:textView.bounds.height/2)
-gradientLayer.startPoint = CGPoint(x:0.0, y:0)
-gradientLayer.endPoint = CGPoint(x:1.0, y:0)
+PlaygroundPage.current.liveView = liveView
 
 
-/*let moveAnimation = CABasicAnimation(keyPath: "startPoint")
-moveAnimation.fromValue = gradientLayer.position
-moveAnimation.toValue = CGPoint(x:textView.bounds.width + gradientLayer.bounds.width/2, y:textView.bounds.height/2)
-moveAnimation.duration = 2.5
-moveAnimation.repeatCount = 7.0
-gradientLayer.add(moveAnimation, forKey: "startPoint")*/
-
-gradientLayer.mask = textLayer
-textView.layer.addSublayer(gradientLayer)
-
-
-PlaygroundPage.current.liveView = textView
 
